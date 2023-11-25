e84cb9db61f56406d98551c28da9eea7deddab83
khanhcoder123 <107303411+khanhcoder123@users.noreply.github.com>
2023-11-25 10:56:25 +0700

    code
    code

---
+++ Tranning/Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Tranning.DataDBContext;
+using Tranning.Models;
+
+namespace Tranning.Controllers
+{
+    public class CourseController : Controller
+    {
+
+        private readonly TranningDBContext _dbContext;
+        public CourseController(TranningDBContext context)
+        {
+            _dbContext = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            CourseDetail course = new CourseDetail();
+            var categoryList = _dbContext.Categories
+            .Where(m => m.deleted_at == null)
+             .Select(m => new SelectListItem { Value = m.id.ToString(), Text = m.name }).ToList();
+            ViewBag.Stores = categoryList;
+            return View(course);
+        }
+    }
+}
---
+++ Tranning/Models/CourseModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tranning.Validations;
+namespace Tranning.Models
+{
+    public class CourseModel
+    {
+        public List<CourseDetail>CourseDetailLists { get; set; }
+    }
+
+    public class CourseDetail
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage = "Enter category, please")]
+        public int category_id { get; set; }
+
+        [Required(ErrorMessage = "Enter name, please")]
+        public string name { get; set; }
+
+        public string? description { get; set; }
+
+        public DateTime start_date {  get; set; }
+        public DateTime? end_date { get; set; }
+        public int? vote {  get; set; }
+        public string avatar {  get; set; }
+
+        [Required(ErrorMessage = "Choose Status, please")]
+        public string status { get; set; }
+
+        [Required(ErrorMessage = "Choose file, please")]
+        [AllowedExtensionFile(new string[] { ".png",".jpg",".jpeg" })]
+        [AllowedSizeFile(3*1024*1024)]
+        public IFormFile? Photo { get; set; }
+
+        public DateTime? created_at { get; set; }
+
+        public DateTime? updated_at { get; set; }
+
+        public DateTime? deleted_at { get; set; }
+    }
+}
